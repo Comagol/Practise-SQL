c1a52cdd42603ebd0afb7dd347ae33e9c588e9b4
comagol <comabellafrancisco@gmail.com>
2026-04-23 10:25:47 -0300

    Add new SQL examples for creating and manipulating brick collections, including table creation, data insertion, and set operations using UNION. This update enhances the practice file with practical applications for data analysis and SQL operations.

--- Exercise oracle gym dev/Union, minus & Intersect.sql
+++ Exercise oracle gym dev/Union, minus & Intersect.sql
@@ -1,1 +1,54 @@
+create table my_brick_collection (
+colour varchar2(10),
+shape  varchar2(10),
+weight integer
+);
+
+drop table your_brick_collation cascade constraints;
+create table your_brick_collection (
+  height integer,
+  width  integer,
+  depth  integer,
+  colour varchar2(10),
+  shape  varchar2(10)
+);
+
+insert into my_brick_collection values ( 'red', 'cube', 10 );
+insert into my_brick_collection values ( 'blue', 'cuboid', 8 );
+insert into my_brick_collection values ( 'green', 'pyramid', 20 );
+insert into my_brick_collection values ( 'green', 'pyramid', 20 );
+insert into my_brick_collection values ( null, 'cuboid', 20 );
+
+insert into your_brick_collection values ( 2, 2, 2, 'red', 'cube' );
+insert into your_brick_collection values ( 2, 2, 2, 'blue', 'cube' );
+insert into your_brick_collection values ( 2, 2, 8, null, 'cuboid' );
+
+commit;
+
+SELECT * FROM my_brick_collection;
+
+
+SELECT * FROM your_brick_collection;
+
+--The set operator Union, intersect and minus allows us to combine many tables into one
+
+--UNION
+-- these operator combine many table into one
+--To use it the tables must have the same number of columns with matching data types in both positions
+
+--in these example it will throws an error (*). Because the table has mismatches in their columns
+SELECT * FROM MY_BRICK_COLLECTION
+UNION
+SELECT * FROM YOUR_BRICK_COLLECTION;
+
+
+--but if we use the colour and shape colum we can use the operator
+SELECT colour, shape FROM my_brick_collection
+UNION
+SELECT colour, shape FROM your_brick_collection;
+
+
+
+
+
 
